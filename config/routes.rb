47a24d116bb3e6e345451c0f0e72cba07ebb4d27
Luiz Affonso <luizbuchhalter@outlook.com>
2021-06-29 01:04:01 -0300

    Finishes modeling the product model with user and add routes to tokens

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
+      resoruces :tokens, only: %i[create]
+      resoruces :products, only: %i[show index]
     end
   end
 end
