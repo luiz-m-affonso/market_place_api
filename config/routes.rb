de0f161d30d61c1e3bb81c92980b69235d98c47a
Luiz Affonso <luizbuchhalter@outlook.com>
2021-06-29 01:30:19 -0300

    Add JWT and config routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
-      resoruces :tokens, only: %i[create]
-      resoruces :products, only: %i[show index]
+      resources :tokens, only: %i[create]
+      resources :products, only: %i[show index create]
     end
   end
 end
--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -7,14 +7,40 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should show product' do
     get api_v1_product_url(@product), as: :json
-    assert_response :sucess
+    assert_response :success
 
     json_response = JSON.parse(self.response.body)
     assert_equal @product.id, json_response['title']
   end
 
   test 'should show products' do
-    get api_v1_products, as: :json
-    assert_response :sucess
+    get api_v1_products_url(), as: :json
+    assert_response :success
+  end
+
+  test 'should create product' do
+    assert_difference("Product.count") do
+      post api_v1_products_url,
+      params: { product: {
+        title: @product.title,
+        price: @product.price,
+        published: @product.published
+      }},
+      headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id)},
+      as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should forbid create product' do
+    assert_no_difference('Product.count') do
+      post api_v1_products_url,
+        params: { product: {
+          title: @product.title,
+          price: @product.price,
+          published: @product.published
+        } }, as: :json
+    end
+    assert_response :forbidden
   end
 end
