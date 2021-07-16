06a5b14e847d461fde590d24c3e9f781671ba357
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-15 23:03:18 -0300

    Add update and destroy methods

--- Gemfile
+++ Gemfile
@@ -40,3 +40,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "bcrypt", "~> 3.1"
+
+gem 'jwt'
+
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
   before_action :check_login, only: %i[create]
+  before_action :check_owner, only: %i[update]
+  before_action :set_product, only: %i[show update destroy]
 
   def show
     render json: Product.find(params[:id])
@@ -18,9 +20,31 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head 204
+  end
+
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def check_owner
+    head :forbidden unless @product.user_id == current_user&.id
+  end
 
   def product_params
     params.require(:product).permit(:title, :price, :published)
   end
+
 end
